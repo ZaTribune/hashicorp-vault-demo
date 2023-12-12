791c71dc091974e4b2b0d068a90a01134b591aa3
Muhammad Ali <38342238+ZaTribune@users.noreply.github.com>
2023-12-12 08:02:46 +0400

    Update Country.java

--- src/main/java/com/tribune/demo/vault/db/entity/Country.java
+++ src/main/java/com/tribune/demo/vault/db/entity/Country.java
@@ -1,12 +1,13 @@
 package com.tribune.demo.vault.db.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
+import java.time.ZonedDateTime;
 
 @Getter
 @Setter
@@ -15,11 +16,29 @@ import lombok.*;
 @Builder
 @ToString
 @Entity
+@Table(name = "Country", indexes = {
+        @Index(name = "idx_country_name_unq", columnList = "name", unique = true)
+})
 public class Country {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
+
+    private String description;
+
+
+    @CreationTimestamp
+    @Column(name = "created_date", nullable = false, updatable = false, columnDefinition = "timestamp default current_timestamp")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:ss Z")
+    private ZonedDateTime createdDate;
+
+
+    @UpdateTimestamp
+    @Column(name = "last_modified_date", nullable = false, columnDefinition = "timestamp default current_timestamp")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:ss Z")
+    private ZonedDateTime lastModifiedDate;
+
 }
