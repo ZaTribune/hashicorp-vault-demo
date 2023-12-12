97dd24c64d5232352baed781097dc66a55800637
Muhammad Ali <38342238+ZaTribune@users.noreply.github.com>
2023-12-12 08:04:28 +0400

    Update MainController.java

--- src/main/java/com/tribune/demo/vault/controller/MainController.java
+++ src/main/java/com/tribune/demo/vault/controller/MainController.java
@@ -2,20 +2,22 @@ package com.tribune.demo.vault.controller;
 
 
 import com.tribune.demo.vault.db.entity.Country;
+import com.tribune.demo.vault.db.entity.CountryInfo;
 import com.tribune.demo.vault.db.repository.CountryRepository;
+import com.tribune.demo.vault.domain.AddCountryRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/test")
+@RequestMapping("/countries")
 @RestController
-public class MainController {
+public class CountryController {
 
 
     private final CountryRepository repository;
@@ -23,6 +25,21 @@ public class MainController {
     @GetMapping
     public List<Country> getCountries() {
         return repository.findAll();
+    }
+
+    @GetMapping("/names")
+    public Collection<CountryInfo> getCountryNames() {
+        return repository.findAllByNameStartingWith("");
+    }
+
+
+    @PostMapping("/country")
+    public Country addCountry(@RequestBody @Valid AddCountryRequest request) {
+        Country country = Country.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .build();
+        return repository.save(country);
     }
 
 }
